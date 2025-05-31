502ec795aec1fba0ebd9676e5ee9abc58f77b7c7
eke vera esther <ekeveraesther@gmail.com>
2025-05-31 23:45:36 +0100

    javascript for interaction

---
+++ index.js
@@ -0,0 +1,6 @@
+
+function changeBackgroundColor() {
+  const colors = ['#FFDDC1', '#C1E1C1', '#C1D4E3', '#F3C1C6', '#E1C1F3'];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.backgroundColor = randomColor;
+}
