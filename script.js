c006cd250256e9d182091448e4ac0a676e744664
eke vera esther <ekeveraesther@gmail.com>
2025-06-02 21:57:03 +0100

    js c

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+const button = document.getElementById('colorButton');
 
-function changeBackgroundColor() {
+button.addEventListener('click', function () {
   const colors = ['#FFDDC1', '#C1E1C1', '#C1D4E3', '#F3C1C6', '#E1C1F3'];
   const randomColor = colors[Math.floor(Math.random() * colors.length)];
   document.body.style.backgroundColor = randomColor;
-}
+});
